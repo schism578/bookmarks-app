a381a8fae1cb44d9d11987b0c8c3cd3f11a4c3d8
Adrian Marquis <adrian.marquis78@gmail.com>
2020-08-20 14:12:36 -0400

    preparing to apply context and API

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: 'AIzaSyAj_yC2L365-0DjE-Ab4CK5NcGG4FG7qTI',
 }
